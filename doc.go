72ea3cf4c54f0045a19d13c7f2976e280ec769a7
Maxwell Harley <maxh@maxh.io>
2019-01-01 20:52:03 -0500

    Hopefully fixed formatting in doc.go

--- doc.go
+++ doc.go
@@ -3,21 +3,21 @@ Package winlog parses Windows Logs using winevt.dll
 
 Example:
 
-    func ExampleNewWinLogWatcher() {
-    	watcher, _ := winlog.NewWinLogWatcher()
-    	// Recieve any future messages
-    	watcher.SubscribeFromNow("Application")
-    	for {
-    		select {
-    		case evt := <-watcher.Event():
-    			// Print the event struct
-    			fmt.Printf("Event: %v\n", evt)
-    			// Print the updated bookmark for that channel
-    			fmt.Printf("Bookmark XML: %v\n", evt.Bookmark)
-    		case err := <-watcher.Error():
-    			fmt.Printf("Error: %v\n\n", err)
-    		}
-    	}
-    }
+	func ExampleNewWinLogWatcher() {
+		watcher, _ := winlog.NewWinLogWatcher()
+		// Recieve any future messages
+		watcher.SubscribeFromNow("Application")
+		for {
+			select {
+			case evt := <-watcher.Event():
+				// Print the event struct
+				fmt.Printf("Event: %v\n", evt)
+				// Print the updated bookmark for that channel
+				fmt.Printf("Bookmark XML: %v\n", evt.Bookmark)
+			case err := <-watcher.Error():
+				fmt.Printf("Error: %v\n\n", err)
+			}
+		}
+	}
 */
 package winlog
